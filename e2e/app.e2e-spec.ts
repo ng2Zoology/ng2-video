0944aa745914e7ea13e527a5ea34ac3a9f6b4afb
Liberty Chisasuro <libertychisasuro@yahoo.com>
2017-07-17 15:01:59 -0400

    Fixed angular-cli tooling

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Ng2VideoPage } from './app.po';
 
-describe('ng2-video App', () => {
+describe('ng2-video App', function() {
   let page: Ng2VideoPage;
 
   beforeEach(() => {
     page = new Ng2VideoPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    expect(page.getParagraphText()).toEqual('app works!');
   });
 });
