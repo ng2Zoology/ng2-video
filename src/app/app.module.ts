7ca3ba4983100d8f0c70bfa901124f540e230cee
Liberty Chisasuro <libertychisasuro@yahoo.com>
2017-07-22 17:17:12 -0400

    Added currentTime to controls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import { VideoModule } from './video/video.module'
 
 // components
 import { AppComponent } from './app.component'
+import { NavComponent } from './nav/nav.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
